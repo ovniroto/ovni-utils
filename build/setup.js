954bb7009eb7617dfb6c1bbe1ab9b5f8e4931f37
Lucas O. S <ovniroto@gmail.com>
2023-09-23 01:31:55 +0200

    chore: update build setup

--- build/setup.js
+++ build/setup.js
@@ -1,14 +1,14 @@
 import { readFile, writeFile } from 'fs';
 
-const file = './src/index.ts';
-const newFile = './src/bundle.ts';
+const base = './src/index.ts';
+const bundle = './src/bundle.ts';
 
-readFile(file, 'utf8', (err, data) => {
+readFile(base, 'utf8', (err, data) => {
     if(err) { console.error(err); return; }
 
     const modifiedData = data.replace(/\.ts/g, "");
 
-    writeFile(newFile, modifiedData, 'utf8', (err) => {
+    writeFile(bundle, modifiedData, 'utf8', (err) => {
         if(err) { console.error(err); return; }
         console.log('Bundle file created!');
     });
